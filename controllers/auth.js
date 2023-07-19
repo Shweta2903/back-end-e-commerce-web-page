f050ae133463bcb4838f1984791cc0b4f3935872
Shweta2903 <shwetasondagar29@gmail.com>
2023-07-19 23:09:32 +0530

    signup works and data dave in DB

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,21 @@
+const User = require("../models/user");
+
 exports.signout = (req, res) => {
-  console.log(req.body);
-  res.json({
-    message: "user Signout success",
-  });
+  console.log("user signout");
 };
 
-exports.signup = (req, res) => {
-  res.json({
-    message: "user signup successfully",
+exports.signup = (req, res, next) => {
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        message: "Not able to save user in DB",
+      });
+    }
+    res.json({
+      name: user.name,
+      email: user.email,
+      id: user._id,
+    });
   });
 };
